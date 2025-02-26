0e9271ee9dd8e70810a85294e22ecd5a92d2574b
PetraGuy99 <petra@rhizocore.com>
2025-02-26 14:02:46 +0000

    fordie
    last work on fordie redo

--- FordieEMF.R
+++ FordieEMF.R
@@ -306,10 +306,10 @@ no_singletons.B <- cbind(reads_filtered.2[, 1, drop = FALSE],
 #FIRST STEP IS TO MUNGE THE BLAST DATA INTO A NICE CSV
  
 #read in first blast shhet
-blast1 = read_excel('../../data/blast.xlsx', sheet = 'blast1', col_names = F)
+blast = read_excel('../../data/blast.xlsx', sheet = 'blast5', col_names = F)
 
 #1. remove rows starting 'Length','Score','Identities','Strand','Sbjct','Query:'  
-filter1 =blast1[!grepl("^(Length|Score|Identities|Strand|Sbjct|Query:)", blast1$...1), ]
+filter1 =blast[!grepl("^(Length|Score|Identities|Strand|Sbjct|Query:)", blast$...1), ]
 
 #remove rows where all cols are NA - these were various text lines, unless they are the OTU number rows
 filter2 <- filter1[rowSums(is.na(filter1[, 2:13])) < ncol(filter1[, 2:13]) | grepl("^Query \\d+ of \\d+: OTU_", filter1[[1]]), ]
@@ -339,10 +339,10 @@ filter4 <- filter3[!grepl("^Reference", filter3[[1]]), ]
 filter5 = filter4[,c(1,2,5,8,14)]
 
 #add some nice colnames
-colnames(filter5) = c('Reference','SH','Genus','Percent','OTU')
+colnames(filter5) = c('Reference','SH','Taxon','Percent','OTU')
 
 #split name into genus and spp
-filter5$species <- sapply(strsplit(filter5$Genus, " "), function(x) ifelse(length(x) > 1, x[2], NA))  # Assign the second word to Species if it exists
+filter5$species <- sapply(strsplit(filter5$Taxon, " "), function(x) ifelse(length(x) > 1, x[2], NA))  # Assign the second word to Species if it exists
 
 #remove rows where spp col is NA
 filter6 <- filter5[!is.na(filter5$species), ]
@@ -351,27 +351,52 @@ filter6 <- filter5[!is.na(filter5$species), ]
 #remove rows <97%
 filter7 = filter6 %>% filter(Percent>=97)
 
-#check the OTUs where there are multiple matches
+#save this for reference
+write.csv(filter7, '../../results/blast597.csv')
+
+ #check the OTUs where there are multiple matches
 library(dplyr)
 
 #these are the OTUs where there are multiple spp matches - but NOT at the same %
-#so that the top % match can be chosen 
+#so that the top % match can be chosen
+#NB - here multiple spp level matches of Laccaria say, at the same % are lost.
+#So if you had L.laccata and L.amethystina both at 99% - they are lost
+#Here we are extracting multiple genus level, of different genus
 df_filtered <- filter7 %>%
   group_by(OTU) %>%
-  filter(n_distinct(Genus) > 1) %>%  # Keep only OTUs where Genus values do not match
+  filter(n_distinct(Taxon) > 1) %>%  # Keep only OTUs where Taxon name values DO NOT match
   filter(n_distinct(Percent) > 1) %>%  # Keep only OTUs where there is variation in Percent
   ungroup()
 
-#now take the top percent match of the multiple matches
+#now all take the top percent match of the multiple matches
 df_top_percent <- df_filtered %>%
   group_by(OTU) %>%
-  slice(which.max(Percent)) %>%  # Select the row with the highest Percent value for each OTU
+  filter(Percent == max(Percent)) %>%  # Select all rows with the highest Percent per OTU
+  ungroup()
+
+#now - IF within those same % values filtered out - all the Taxon names are the same - keep, 
+#else save to a different df to peruse later
+df_top_percent_clean <- df_top_percent %>%
+  group_by(OTU) %>%
+  filter(n_distinct(Taxon) == 1) %>%  # Keep only OTUs where all Taxon values match
+  ungroup()
+
+#now there are still multiple rows per OTU, we need one row per OTU
+df_top_item <- df_top_percent_clean %>%
+  group_by(OTU) %>%
+  slice_max(order_by = Percent, n = 1, with_ties = FALSE) %>%  # Keep only the highest Percent per OTU
+  ungroup()
+#the lost OTUs where multiple spp have same %
+df_multiple_taxa <- df_top_percent %>%
+  group_by(OTU) %>%
+  filter(n_distinct(Taxon) > 1) %>%  # Keep OTUs where Taxon varies
   ungroup()
 
 #then the OTUs which didnt have multiple matches - were all the same species for each OTU
+#doesnt matter if same spp same % or same spp different %, just take top row
 df_same_spp <- filter7 %>%
   group_by(OTU) %>%
-  filter(n_distinct(Genus) == 1) %>%  # Keep OTUs where all Genus values are the same
+  filter(n_distinct(Taxon) == 1) %>%  # Keep OTUs where all Taxon values are the same
   ungroup()
 
 #and then take the first row
@@ -380,4 +405,268 @@ df_first_value <- df_same_spp %>%
   slice_head(n = 1) %>%  # Select the first row for each OTU
   ungroup()
 
-blast1_OTUs = rbind.data.frame(df_first_value, df_top_percent)
+
+blast_OTUs = rbind.data.frame(df_first_value, df_top_item)
+write.csv(blast_OTUs, '../../results/blast5_OTUs.csv')
+
+#get the OTUs where taxon names was different - but percent match was the same - leave these out
+#but what were they
+df_filtered_out <- filter7 %>%
+  group_by(OTU) %>%
+  filter(n_distinct(Taxon) > 1) %>%  # Keep only OTUs where taxon name is different
+  filter(n_distinct(Percent) == 1) %>%  # Keep only OTUs where Percent has no variation
+  ungroup()
+
+#all lost OTU = 
+blast_OTUs_lost = rbind.data.frame(df_filtered_out, df_multiple_taxa)
+write.csv(df_filtered_out,'../../results/blast5_OTUs_lost.csv')
+
+
+#Now to combine all the OTUs into one vector and merge with the reads table - because the
+#reads table has locations for each OTU. The matched OTUs we have selected to be >97% etc
+#will be a subset of all OTUs contained in the reads table.
+#Therefor - select the columns of the reads table where the OTU appears in our OTU set
+
+#first collect all our matched OTUs
+#path to excel data
+file_path <- "../../data/blast.xlsx"
+
+# Generate sheet names dynamically
+sheet_names <- paste0("blast", 1:5, "_matches")
+
+# Read all sheets into a list of data frames
+blast_data <- lapply(sheet_names, read_excel, path = file_path, col_names = TRUE)
+
+#combine into single df
+blast_combined <- do.call(rbind, blast_data)
+
+#read in the reads table
+reads = read_xlsx('../../data/Fordie_eDNA_Data.xlsx',sheet = 'reads', col_names = T)
+
+#tidy
+reads =reads[,-c(1,3)]
+
+#check - there are no cols where colsum = 1, i.e. no singletons (those entries with 1 read)
+# Compute column sums
+col_sums <- colSums(reads[,-1], na.rm = TRUE)
+
+# Check if any column sum equals 1
+any(col_sums == 1) #FALSE, i.e. yes - there are no singletons   
+
+#checking low read occurrences
+#what do reads per sample look like
+#add col of total reads per sample
+RowSum <- rowSums(reads[, -1], na.rm = TRUE)
+#row 2 and 3 have unusually low reads
+
+#examine row 2
+row3_data <- as.numeric(reads[3, -1])  
+
+row_data <- row3_data[row3_data != 0]
+
+# Plot histogram
+hist(row_data, breaks = breaks, main = "Histogram of Values at location S001Z0008", 
+     xlab = "Value", ylab = "Frequency", col = "blue", border = "black", 
+     xaxt = "n")  # Disable default x-axis ticks
+
+# Add custom x-axis ticks
+axis(1, at = 1:38, labels = 1:38)
+
+#Site S001Z0007 has a low total read abundance, with one entry of 38 - look at this OTU
+column_index <- which(reads[2, -1] == 38)
+
+# Get the column name
+column_name <- colnames(reads[column_index + 1] ) # Adding 1 to account for excluding the first column
+column_name #+ OTU1843, this is a common sapro, no reason to think its weird, so leaving this in
+
+#examine row 3
+
+row_data <- row2_data[row2_data != 0]
+
+breaks <- seq(1, 38, by = 1)
+
+# Plot histogram
+hist(row_data, breaks = breaks, main = "Histogram of Values at location S001Z0007", 
+     xlab = "Value", ylab = "Frequency", col = "blue", border = "black", 
+     xaxt = "n")  # Disable default x-axis ticks
+
+
+#Site S001Z0007 has a low total read abunance, with one entry of 38 - look at this OTU
+column_index <- which(reads[2, -1] == 1)
+
+# Get the column name
+column_name <- colnames(reads[column_index + 1] ) # Adding 1 to account for excluding the first column
+column_name #OTU0002,0013 and 0071 - none of these in our matched OTUs, so this sample will
+#fall out of data set anyway
+
+
+#######end of checking low read abundance etc#########################
+
+# Load functional traits data
+functionaltraits <- read.csv('../../data/FunctionalTraits.csv')
+
+#split the taxon column into genus and species so that it can be merged with functional traits
+# Split Taxon column by space
+split_taxon <- strsplit(as.character(blast_combined$Taxon), " ")
+
+# Extract Genus (first element) and Species (second element, if present)
+blast_combined$GENUS <- sapply(split_taxon, `[`, 1)
+blast_combined$Species <- sapply(split_taxon, `[`, 2)
+
+
+# Merge species data with functional traits, keeping all species from species_df_species level
+blast_functional_traits <- blast_combined %>%
+  left_join(functionaltraits %>% select(GENUS, primary_lifestyle), by = "GENUS")
+
+#Make changes to funcitonal traits where some species arent in the database
+#see data processing file for details
+#1. 
+# Update primary_lifestyle where Taxon is 'Podila humilis'
+blast_functional_traits <- blast_functional_traits %>%
+  mutate(primary_lifestyle = ifelse(Taxon == "Podila humilis", "soil_saprotroph", primary_lifestyle))
+
+#2.
+#Update primary_lifestye where Taxon is 'Entomortierella jenkinii'
+blast_functional_traits <- blast_functional_traits %>%
+  mutate(primary_lifestyle = ifelse(Taxon == 'Entomortierella jenkinii', "soil_saprotroph", primary_lifestyle))
+
+#3.
+#Update primary_lifestye where Taxon is 'Pseudoschizothecium atropurpureum
+blast_functional_traits <- blast_functional_traits %>%
+  mutate(primary_lifestyle = ifelse(Taxon == 'Pseudoschizothecium atropurpureum', "soil_saprotroph", primary_lifestyle))
+
+#4.
+#Update primary_lifestye where Taxon is 'Dissoderma galerinicola
+blast_functional_traits <- blast_functional_traits %>%
+  mutate(primary_lifestyle = ifelse(Taxon == 'Dissoderma galerinicola', "unspecified_saprotroph", primary_lifestyle))
+
+#5
+#delete the unknowns and weird
+# Define OTUs to remove
+otus_to_remove <- c("OTU_632", "OTU_2511", "OTU_1942", "OTU_2183",'OTU_4340')
+
+# Remove rows where OTU is in otus_to_remove
+blast_functional_traits <- blast_functional_traits[!blast_functional_traits$OTU %in% otus_to_remove, ]
+
+#blast_functional_traits now has all the OTUs we want to work with for this analysis
+###################################################################################
+
+
+#get OTUs from reads table to match in format to the blast OTU column
+otu_blast = blast_functional_traits$OTU
+otu_blast_fixed1 = sub('OTU_', 'Otu', otu_blast) #remove _ and change case
+otu_blast_fixed2 <- sprintf("Otu%04d", as.numeric(sub("Otu", "", otu_blast_fixed1))) #add the zeros
+
+#now otu_blast_fixed is in the same text format as the colnames in the reads table 
+#can now select only the cols of the reads table which have otus in our blast match
+reads_subset <- reads %>% select(all_of(otu_blast_fixed2))
+
+#stick the locations col back on
+reads_subset = cbind.data.frame(reads[,1], reads_subset)
+
+# Load proportions data and convert to presence-absence
+reads_pres_abs <- reads_subset %>%
+  mutate(across(-1, ~ ifelse(. > 0, 1, 0)))  # Apply transformation to all columns except the first
+
+#Change from Otu to species hypothesis in the reads subset table for final analysis
+#oops change the col names match functional traits
+#bit circular here :)
+# Rename columns in reads_subset to match OTU format in blast_functional_traits
+colnames(reads_pres_abs)[-1] <- gsub("Otu0*", "OTU_", gsub("Otu", "OTU_", colnames(reads_pres_abs)[-1]))
+
+# Remove any remaining leading zeros in the numeric part
+colnames(reads_pres_abs)[-1] <- gsub("OTU_0*", "OTU_", colnames(reads_pres_abs)[-1])
+
+#change to taxa
+otu_to_taxon <- setNames(blast_functional_traits$Taxon, blast_functional_traits$OTU)
+
+# Rename matching columns in reads_pres_abs
+colnames(reads_pres_abs)[-1] <- ifelse(colnames(reads_pres_abs)[-1] %in% names(otu_to_taxon),
+                                       otu_to_taxon[colnames(reads_pres_abs)[-1]],
+                                       colnames(reads_pres_abs)[-1])
+
+###
+#duplicated colnames - because several OTUs are the same spp there are duplicated 
+#cols in the reads_pres_abs df
+# Assuming your dataframe is called df
+duplicated_columns <- colnames(reads_pres_abs)[duplicated(colnames(reads_pres_abs))]
+
+# Check the result
+duplicated_columns
+
+#these might take care of themselves - but we will have a many to many match below
+#in the merge which we will need to adjust
+
+#check for all zero cols
+# Identify columns that contain only zeros
+all_zero_cols <- colSums(reads_pres_abs[-1] == 0) == nrow(reads_pres_abs)
+
+# Print column names that are all zeros
+names(all_zero_cols[all_zero_cols])
+#none - i.e. all the species hypotheses we have occur at at least one site
+
+#now create list by site#######################
+
+#need to only select the unique taxa from blast_functional_traits otherwise the merge 
+#is confused - i.e. the blast_funtional_traits has a row for every OTU, where multiple
+#OTU map to the same tax
+
+unique_blast_functional <- blast_functional_traits %>%
+  distinct(Taxon, .keep_all = TRUE)
+
+# Function to process each row of the data
+process_row <- function(row) {
+  # Clean the row by removing columns with value 0
+  row_cleaned <- row[, row[1, ] != 0]
+  
+  # Transpose the cleaned row and set column names
+  row_cleaned <- as.data.frame(t(rbind(colnames(row_cleaned), row_cleaned)), row.names = FALSE)
+  colnames(row_cleaned) <- as.character(row_cleaned[1, ])
+  row_cleaned <- row_cleaned[-1, ]  # Remove the first row (which is now the column names)
+  
+  #there will be multiple entreies here for the OTUs where there is many to one relationship -
+  #eg, Podila humilis which is multiple OTUs.
+  #The merge will then create Podila humils, Podilia humilis.1 etc if both OTUs occur
+  #If both occur - we only need the first instance because they are the same spp -
+  #so delete any .
+  row_cleaned_duplicates <- row_cleaned[!grepl("\\.\\d+$", row_cleaned$Group), ]
+ 
+  colnames(row_cleaned_duplicates)[1] = 'Taxon' #change for merge
+  
+  # Merge with functional traits data and select necessary columns
+  # Assuming your dataframe is called df_cleaned_duplicates and unique_blast_functional
+  merged_df <- merge(row_cleaned_duplicates, unique_blast_functional[, c("Taxon", "primary_lifestyle")], 
+                     by = "Taxon", 
+                     all.x = TRUE)  
+  
+  return(merged_df)
+}
+
+species_by_site <- apply(reads_pres_abs, 1, function(process_row) {
+  
+  # Identify species (column names) present at the site (i.e., where value is 1, not NA)
+  species_present <- colnames(reads_pres_abs)[process_row == 1]
+  
+  # For each present species, get its primary_lifestyle from unique_blast_functional
+  species_lifestyle <- unique_blast_functional[unique_blast_functional$Taxon %in% species_present, c("Taxon", "primary_lifestyle")]
+  
+  # Create a data frame of species and primary_lifestyle for this site
+  result <- data.frame(Species = species_lifestyle$Taxon, 
+                       Primary_Lifestyle = species_lifestyle$primary_lifestyle)
+  
+  
+  
+  return(result)
+})
+
+  
+site_names <- reads_pres_abs$Group  # Assuming 'Group' is the name of the column for site names
+
+# Convert the resulting list of dataframes into a named list of dataframes
+species_by_site_named <- setNames(species_by_site, site_names)
+
+write_xlsx(species_by_site_named, path = '../../results/Fordiespecies2.xlsx')
+
+
+
+
